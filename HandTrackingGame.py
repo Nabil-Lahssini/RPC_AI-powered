e37173497e2e64dc6f7d653b59d71f77d441d98c
nabil.lahssini <nabil.lahssini@student.ehb.be>
2021-09-30 19:32:54 +0200

    live recognition

--- HandTrackingGame.py
+++ HandTrackingGame.py
@@ -3,7 +3,7 @@ import time
 import sys
 import keyboard
 from HandTrackingModule import HandDetector
-
+from collections import deque
 
 
 
@@ -15,16 +15,19 @@ def analyze_fingers(sample: int, player) -> list:
     cap = cv2.VideoCapture(0)
     pTime = 0
     cTime = 0
-    instance = HandDetector(maxHands=1)
-    tipIds = [4, 8, 12, 16, 20]
-    frequency = []
+    instance = HandDetector(maxHands=2)
+    tipIds = [4, 8, 12, 16, 20, 24, 28, 32, 36, 40]
+    frequency = deque(maxlen=sample)
+    frequency2 = deque(maxlen=sample)
     item = ""
+    item2 = ""
     while True:
         success, img = cap.read()
         img = instance.find_hands(img, draw=True)
         lmList = instance.find_position(img)
         if len(lmList) is not 0:
             fingers = []
+            fingers2 = []
             # Thumb
             if lmList[tipIds[0]][1] < lmList[tipIds[0] - 1][1]:
                 fingers.append(1)
@@ -36,18 +39,31 @@ def analyze_fingers(sample: int, player) -> list:
                     fingers.append(1)
                 else:
                     fingers.append(0)
+            if len(lmList) >40:
+                # Thumb
+                if lmList[tipIds[5]][1] < lmList[tipIds[5] - 1][1]:
+                    fingers.append(1)
+                else:
+                    fingers.append(0)
+                for id in range(6, 10):
+                    if lmList[tipIds[id]][2] < lmList[tipIds[id] - 2][2]:
+                        fingers2.append(1)
+                    else:
+                        fingers2.append(0)
+                totalFingers2 = fingers2.count(1)
+                frequency2.append(totalFingers2)
             totalFingers = fingers.count(1)
             frequency.append(totalFingers)
-            if len(frequency) > sample:
-                frequency = []
         if len(frequency) == sample:
             item = translate(most_frequent(frequency))
+        if len(frequency2) == sample:
+            item2 = translate(most_frequent(frequency2))
         cv2.putText(img, item, (10, 100), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
-        cv2.putText(img, f"player {str(player)}:", (10, 65), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
+        cv2.putText(img, item2, (10, 65), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 255), 3)
         cv2.imshow("Image", img)
         cv2.waitKey(1)
         if keyboard.is_pressed("space"):
-            return item
+            return item, item2
 
 def translate(number):
     if number == 2 or number == 3:
@@ -59,7 +75,7 @@ def translate(number):
 
 def logic(player1, player2):
     if player1 is player2:
-        return "Equal"
+        return "Nobody"
 
     if player1 == "schaar" and player2 == "papier":
         return "player 1"
@@ -78,8 +94,7 @@ def logic(player1, player2):
 
 
 def main():
-    player1 = analyze_fingers(10, player=1)
-    player2 = analyze_fingers(10, player=2)
+    player1, player2 = analyze_fingers(10, player=1)
     result = (logic(player1, player2) + " WINS !!")
     cap = cv2.VideoCapture(0)
     instance = HandDetector(maxHands=1)
@@ -91,6 +106,7 @@ def main():
         cv2.waitKey(1)
         if keyboard.is_pressed("space"):
             break
+
 
 
 if __name__ == "__main__":
